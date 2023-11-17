ae1df7a739d777687a1beda13a4206e4992b96ba
penkichkun <151262367+penkichkun@users.noreply.github.com>
2023-11-18 08:43:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from machine import Pin
-AP_btn = Pin(0, Pin.IN, Pin.PULL_UP) # APボタン（アクセスポイント）
+BTN = Pin(0, Pin.IN, Pin.PULL_UP) # APボタン（アクセスポイント）
+LEDR = machine.Pin(13, machine.Pin.OUT) # 赤
 
-if AP_btn.value() == 0:
+
+if BTN.value() == 0:
+    LEDR.value(0)
     import apws_8266
-    apws.main()
+    apws_8266.main()
+
+else:
+    LEDR.value(1)
 
 import mqtt
